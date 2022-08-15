b2e1d570690620e38b814e1c4a18bb8152a8717b
rgavrilov <rgavrilov@cinimex.ru>
2022-08-15 16:46:43 +0200

    Replace Oracle DB with Postgres - Add docker-compose.yml and Dockerfile for WS - Add second spring profile - Fix name of properties class

--- src/main/java/com/example/j4roman/barcode/Starter.java
+++ src/main/java/com/example/j4roman/barcode/Starter.java
@@ -1,30 +1,32 @@
 package com.example.j4roman.barcode;
 
 import com.example.j4roman.barcode.config.HibernateProperties;
-import com.example.j4roman.barcode.config.SpringDataSourceProperies;
+import com.example.j4roman.barcode.config.SpringDataSourceProperties;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableConfigurationProperties({
         HibernateProperties.class,
-        SpringDataSourceProperies.class })
+        SpringDataSourceProperties.class })
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class })
 public class Starter extends SpringBootServletInitializer {
-//    public static void main(String[] args) {
-//        SpringApplication.run(applicationClass, args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
+    }
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(applicationClass);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 
-//    private static Class<Starter> applicationClass = Starter.class;
+    private final static Class<Starter> applicationClass = Starter.class;
 }
